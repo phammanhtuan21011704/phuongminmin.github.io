caf1af54d553c06606e55499e63d526deb2d40af
phammanhtuan21011704 <46343484+phammanhtuan21011704@users.noreply.github.com>
2019-01-17 23:00:58 +0700

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,17 +1,27 @@
 ﻿document.addEventListener("DOMContentLoaded",function(){
-	var anh=document.querySelectorAll('.image');
-	var denhienra=document.querySelector('.mauden');
-	anhphongtos=document.querySelector('.col-xs-6.col-sm-6.col-md-6.col-lg-6.formtrong');
+	var bamanh=document.querySelectorAll('.anh');
+	var hienden=document.querySelector('.mauden');
+	var anhra=document.querySelector('.mautrang');
+	var cacanh=document.querySelectorAll('.mautrang ul li');
+	console.log(cacanh);
+	for (var i = 0; i < bamanh.length; i++) {
+		bamanh[i].addEventListener("click",function(){
+			console.log('kick roi')
+			hienden.classList.add('denra');
+			anhra.classList.add('denra');
+			var anhdckick=(this);
 
-	for (var i = 0; i < anh.length; i++) {
-		anh[i].addEventListener("click",function(){
-			denhienra.classList.add('denra');
-			anhphongtos.classList.add('denra');
-					})
+			for (var chiso = 0;anhdckick=anhdckick.previousElementSibling; chiso++) {	
+			}
+			cacanh[chiso].classList.add('denra');
+		});
 	}
-	denhienra.addEventListener('click',function(){
-		denhienra.classList.remove('denra');
-		anhphongtos.classList.remove('denra');
+	hienden.addEventListener("click",function(){
+		hienden.classList.remove('denra');
+		anhra.classList.remove('denra');
+		var tat=document.querySelector('.mautrang ul li.denra');
+		tat.classList.remove('denra');
 
 	})
+	
 },false);
