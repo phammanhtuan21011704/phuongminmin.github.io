4ffcb882048afad445c5797b32b46a5836b9c31e
phammanhtuan21011704 <46343484+phammanhtuan21011704@users.noreply.github.com>
2019-01-17 16:49:01 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+	var anh=document.querySelectorAll('.image');
+	var denhienra=document.querySelector('.mauden');
+	anhphongtos=document.querySelector('.col-xs-6.col-sm-6.col-md-6.col-lg-6.formtrong');
+
+	for (var i = 0; i < anh.length; i++) {
+		anh[i].addEventListener("click",function(){
+			denhienra.classList.add('denra');
+			anhphongtos.classList.add('denra');
+					})
+	}
+	denhienra.addEventListener('click',function(){
+		denhienra.classList.remove('denra');
+		anhphongtos.classList.remove('denra');
+
+	})
+},false);
